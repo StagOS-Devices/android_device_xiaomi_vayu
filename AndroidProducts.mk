c09174fdb8ed58fe67e61efef679df2f3072d452
KangMonkey <psambitk1@gmail.com>
2021-12-29 04:02:32 +0000

    vayu: SparkOS [OFFICIAL]

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_vayu.mk
+    $(LOCAL_DIR)/spark_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_vayu-eng \
-    arrow_vayu-userdebug \
-    arrow_vayu-user
+    spark_vayu-eng \
+    spark_vayu-userdebug \
+    spark_vayu-user
--- spark_vayu.mk
+++ spark_vayu.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/spark/config/common.mk)
 
-PRODUCT_NAME := arrow_vayu
+PRODUCT_NAME := spark_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -28,6 +28,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=M2102J20SI
 
 BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
-
-DEVICE_MAINTAINER := kubersharma001
-
+TARGET_BOOT_ANIMATION_RES := 1080
+# Spark Official
+SPARK_BUILD_TYPE := OFFICIAL
